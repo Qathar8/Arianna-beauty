e5c248dfbaa9c0f254c1431eb4718c440310d5d4
Qathar8 <qureshqathar10@gmail.com>
2025-06-20 17:54:50 +0300

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,4 +0,0 @@
-// Updated Supabase client file to use D1 instead
-// This maintains compatibility with existing frontend code
-
-export { supabase, type Product, type Order, type Database } from './d1-client';
